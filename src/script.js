614ebc64e295c0312fe07e56117829f3dac5fe86
Maja Dit <maja.ditrtova@outlook.com>
2023-01-19 22:16:18 +0000

    add fading effect to pojdme se poznat text

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 const rolloutText = document.getElementById("rollout-text");
 const rolloutButton = document.getElementById("poznej-me-vic-button");
+const fading = document.getElementById("fade-out-text");
 
 function show_hide() {
   if (rolloutText.style.display === "none") {
     rolloutText.style.display = "block";
+    fading.classList.remove("closed");
   } else {
     rolloutText.style.display = "none";
+    fading.classList.add("closed");
   }
 }
 rolloutButton.addEventListener("click", show_hide);
