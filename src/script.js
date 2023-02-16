29c9ec4f3dd089e5b29f2e084d803c3765144ad9
Tomáš Hendrych <tomas.hendrych@mallgroup.com>
2023-02-16 23:10:09 +0100

    🎨 hero section cleanup

--- src/script.js
+++ src/script.js
@@ -12,24 +12,3 @@ function show_hide() {
   }
 }
 rolloutButton.addEventListener("click", show_hide);
-
-
-
-// function sendEmail() {
-//   fetch("https://formsubmit.co/ajax/tom.hendrych@seznam.cz", {
-//     method: "POST",
-//     headers: { 
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json'
-//     },
-//     body: JSON.stringify({
-//         name: "FormSubmit",
-//         message: "I'm from Devro LABS"
-//     })
-// })
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(error => console.log(error));
-// }
-
-// sendEmail()
