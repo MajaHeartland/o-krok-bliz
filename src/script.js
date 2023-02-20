1b0602afe51e7018e45abe097d80b307cc244fb0
Tomáš Hendrych <tomas.hendrych@mallgroup.com>
2023-02-20 21:55:17 +0100

    merge

--- src/script.js
+++ src/script.js
@@ -11,28 +11,33 @@ function show_hide() {
     fading.classList.add("closed");
   }
 }
-rolloutButton.addEventListener("click", show_hide);
+// rolloutButton.addEventListener("click", show_hide);
 
-const formButton = document.getElementById('form-button')
-formButton.addEventListener("click", sendEmail);
-console.log(formButton)
+const dogFound = new CustomEvent("test");
 
-function sendEmail() {
-  console.log('called')
-  fetch("https://formsubmit.co/ajax/5dd9c70e831165c738d2000305c22722", {
-    method: "POST",
-    headers: { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-        name: "FormSubmit",
-        message: "Cus Tome!"
-    })
-})
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
-}
+// add an appropriate event listener
+// window.addEventListener("test", (e) => console.log(e));
+
+setTimeout(() => {
+  window.dispatchEvent(dogFound);
+}, 1000)
+
+
+// function sendEmail() {
+//   fetch("https://formsubmit.co/ajax/5dd9c70e831165c738d2000305c22722", {
+//     method: "POST",
+//     headers: { 
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         name: "FormSubmit",
+//         message: "Cus Tome!"
+//     })
+// })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+// }
 
 // sendEmail()
