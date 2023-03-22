2df0c2d3578851f77d4cd4d828384caa7f238808
Maja Dit <maja.ditrtova@outlook.com>
2023-03-22 23:44:11 +0000

    add navigation

--- src/script.js
+++ src/script.js
@@ -14,16 +14,16 @@ function show_hide() {
 rolloutButton.addEventListener("click", show_hide);
 
 /*  toggle navbar visibility */
-// let prevScrollpos = window.pageYOffset;
-// window.onscroll = function () {
-//   let currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos || currentScrollPos < 10) {
-//     document.querySelector("nav").classList.remove("hidden");
-//   } else {
-//     document.querySelector("nav").classList.add("hidden");
-//   }
-//   prevScrollpos = currentScrollPos;
-// };
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos || currentScrollPos < 10) {
+    document.querySelector(".navigation").classList.remove("hidden");
+  } else {
+    document.querySelector("nav").classList.add("hidden");
+  }
+  prevScrollpos = currentScrollPos;
+};
 
 // dropdowns //
 const items = document.querySelectorAll(".accordions");
