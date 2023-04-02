8d6aecdab5f6b2377797863300f87f6c6111f305
Maja Dit <maja.ditrtova@outlook.com>
2023-04-02 14:17:19 +0100

    fix buggy navbar

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ const rolloutText = document.getElementById("rollout-text");
 const rolloutButton = document.getElementById("poznej-me-vic-button");
 const fading = document.getElementById("fade-out-text");
 
+/* pojdme se poznat hide and show text */
 function show_hide() {
   if (rolloutText.style.display === "none") {
     rolloutText.style.display = "block";
@@ -16,11 +17,16 @@ rolloutButton.addEventListener("click", show_hide);
 /*  toggle navbar visibility */
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
+  let navbar = document.getElementById("navbar");
   let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos || currentScrollPos < 10) {
-    document.querySelector(".navigation").classList.remove("hidden");
+  if (window.matchMedia("(min-width: 1280px)").matches) {
+    if (prevScrollpos > currentScrollPos) {
+      navbar.style.top = "0";
+    } else {
+      navbar.style.top = "-100px";
+    }
   } else {
-    document.querySelector("nav").classList.add("hidden");
+    navbar.classList.remove("large-screens");
   }
   prevScrollpos = currentScrollPos;
 };
