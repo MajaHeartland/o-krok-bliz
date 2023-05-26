d239d4d25bcbc2faec06fec915cef1aea55c45a7
Maja Dit <maja.ditrtova@outlook.com>
2023-05-26 21:31:23 +0100

    update form error message

--- src/scripts/Notification.js
+++ src/scripts/Notification.js
@@ -1,16 +1,15 @@
 export default {
-    props: {
-      isError: {
-        type: Boolean,
-        default: false,
-      }
+  props: {
+    isError: {
+      type: Boolean,
+      default: false,
     },
-    data() {
-      return { 
-      }
-    },
-    methods: {},
-    template: `
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  template: `
       <div class="notification" :class="{'notification--error': isError}">
         <div v-if="!isError">
           <div class="notification__title">Úvodní setkání zdarma 🎉 👍 🙌 </div>
@@ -18,9 +17,9 @@ export default {
           <div class="notification__description">Díky za zprávu a do 48 h se Ti ozvu!</div>
         </div>
         <div v-else >
-          <div class="notification__title"><strong>Něco se nepovedlo !!! ❌❌❌</strong></div>
+          <div class="notification__title"><strong>Jejda ❌ Něco se nepovedlo</strong></div>
           <div class="notification__subtitle">Prosím zkuste zprávu odeslat znovu</div>
         </div>
       </div>
-    `
-  }
+    `,
+};
